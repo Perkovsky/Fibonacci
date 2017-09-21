8503801ef25f5a229b3f29d4fca54fe62ae60aeb
Igor Perkovsky <igor.perkovsky@mail.ru>
2017-09-21 11:50:35 +0300

    Fin

--- Fibonacci/Program.cs
+++ Fibonacci/Program.cs
@@ -9,9 +9,7 @@ namespace Fibonacci
         {
             var cache = new Dictionary<T, R>();
             R result = default(R);
-            return x => cache.TryGetValue(x, out result)
-                                ? result
-                                : cache[x] = func(x);
+            return x => cache.TryGetValue(x, out result) ? result : cache[x] = func(x);
         }
     }
 
@@ -19,23 +17,18 @@ namespace Fibonacci
     {
         static void Main(string[] args)
         {
-            //Console.SetWindowSize(100, 58);
-
             Func<ulong, ulong> fib = null;
             fib = x => x > 1 ? fib(x - 1) + fib(x - 2) : x;
             fib = fib.Memoize();
 
-            Func<ulong, decimal> fibConstant = null;
-            fibConstant = x => (x == 1) ? 1 / ((decimal)fib(x)) : 1 / ((decimal)fib(x)) + fibConstant(x - 1);
+            //ulong Fib(ulong x) => x > 1 ? Fib(x - 1) + Fib(x - 2) : x;
 
-            fibConstant = fibConstant.Memoize();
+            for (ulong i = 0; i < 50; i++)
+            {
+                Console.Write($"{fib(i)} ");
+            }
+            Console.WriteLine("Completed!");
 
-            Console.WriteLine("\n{0}\t{1}\t{2}\t{3}\n", "Count", "Fibonacci".PadRight(24), "1/Fibonacci".PadRight(24), "Fibonacci Constant".PadRight(24));
-
-            for (ulong i = 1; i <= 93; ++i)
-                Console.WriteLine("{0:D5}\t{1:D24}\t{2:F24}\t{3:F24}", i, fib(i), (1 / (decimal)fib(i)), fibConstant(i));
-
-            // Задержка.
             Console.ReadKey();
 
         }
